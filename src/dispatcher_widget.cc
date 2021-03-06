69d416243783e71823afb331030bb15c8760415c
Kristopher Wehage <kwehage@jpl.nasa.gov>
2021-03-06 08:44:06 -0800

    Handle sigint, get ros2 graph interface

--- src/dispatcher_widget.cc
+++ src/dispatcher_widget.cc
@@ -2,12 +2,21 @@
 #include "dispatcher/dispatch_item.h"
 #include "dispatcher/config.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <string>
 #include <iostream>
 
 #include <QWidget>
-#include <QVBoxLayout>
+#include <QGridLayout>
 #include <QGroupBox>
+#include <QSpacerItem>
+#include <QSocketNotifier>
+#include <QCoreApplication>
+
+#include <rclcpp/node_interfaces/node_base_interface.hpp>
 
 #include <yaml-cpp/yaml.h>
 
@@ -29,28 +38,83 @@ std::string dispatcher::commit() {
   return std::string(DISPATCHER_GIT_COMMIT);
 }
 
+#if 0
+static int setup_unix_signal_handlers() {
+  struct sigaction hup, term;
+
+  hup.sa_handler = dispatcher::DispatcherWidget::hupSignalHandler;
+  sigemptyset(&hup.sa_mask);
+  hup.sa_flags = 0;
+  hup.sa_flags |= SA_RESTART;
+
+  if (sigaction(SIGHUP, &hup, 0)) {
+    return 1;
+  }
+
+  term.sa_handler = dispatcher::DispatcherWidget::termSignalHandler;
+  sigemptyset(&term.sa_mask);
+  term.sa_flags = 0;
+  term.sa_flags |= SA_RESTART;
+
+  if (sigaction(SIGTERM, &term, 0)) {
+    return 2;
+  }
+
+  return 0;
+}
+#endif
+
 /*!
 @brief class constructor for DispatcherWidget application
 */
 dispatcher::DispatcherWidget::DispatcherWidget(QWidget* parent)
   : QWidget(parent), CasahNode("dispatcher", "dispatcher", 2.0, "/logs", "INFO") {
 
+#if 0
+  setup_unix_signal_handlers();
+
+  // set up signal handling
+  if (socketpair(AF_UNIX, SOCK_STREAM, 0, sighupFd)) {
+    qFatal("Couldn't create HUP socketpair");
+  }
+
+  if (socketpair(AF_UNIX, SOCK_STREAM, 0, sigtermFd)) {
+     qFatal("Couldn't create TERM socketpair");
+  }
+
+  snHup = new QSocketNotifier(sighupFd[1], QSocketNotifier::Read, this);
+  connect(snHup, SIGNAL(activated(QSocketDescriptor)), this, SLOT(handleSigHup()));
+  snTerm = new QSocketNotifier(sigtermFd[1], QSocketNotifier::Read, this);
+  connect(snTerm, SIGNAL(activated(QSocketDescriptor)), this, SLOT(handleSigTerm()));
+#endif
+
+  // declare ros parameters
   this->declare_parameter<std::string>("dispatcher_config_path", "");
   this->get_parameter("dispatcher_config_path", dispatcher_config_path_);
-
-  CFW_INFO("Parsing config file");
-
-  parseConfig(dispatcher_config_path_);
 
   timer_ = new QTimer;
 
   QGroupBox* main_box = new QGroupBox;
   main_box->setContentsMargins(QMargins(0, 0, 0, 0));
 
-  QVBoxLayout* layout = new QVBoxLayout(main_box);
-  layout->setContentsMargins(QMargins(0, 0, 0, 0));
-  layout->setMargin(0);
-  layout->setSpacing(0);
+  layout_ = new QGridLayout(main_box);
+  setLayout(layout_);
+
+  CFW_INFO("Parsing config file: %s", dispatcher_config_path_.c_str());
+  parseConfig(dispatcher_config_path_);
+
+  QSpacerItem* spacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+  layout_->addItem(spacer, layout_->rowCount(), 0);
+
+   // setup signal and slot
+  connect(timer_, SIGNAL(timeout()), this, SLOT(Process())); 
+  double loop_period_ms = 1.0 / target_loop_rate_hz_ * 1000.0;
+  CFW_INFO("Using loop period %f ms", loop_period_ms);
+  timer_->start(loop_period_ms);
+
+  node_graph_ = 
+    std::make_shared<rclcpp::node_interfaces::NodeGraph>(
+      this->get_node_base_interface().get());
 }
 
 void dispatcher::DispatcherWidget::parseConfig(const std::string& config) {
@@ -68,4 +132,42 @@ void dispatcher::DispatcherWidget::parseConfig(const std::string& config) {
 dispatcher::DispatcherWidget::~DispatcherWidget() {}
 
 
-void dispatcher::DispatcherWidget::Process() {}
+void dispatcher::DispatcherWidget::handleSigTerm()
+{
+#if 0
+    snTerm->setEnabled(false);
+    char tmp;
+    read(sigtermFd[1], &tmp, sizeof(tmp));
+
+    // do Qt stuff
+
+    snTerm->setEnabled(true);
+#endif
+}
+
+
+void dispatcher::DispatcherWidget::handleSigHup()
+{
+#if 0
+    snHup->setEnabled(false);
+    char tmp;
+    read(sighupFd[1], &tmp, sizeof(tmp));
+
+    // do Qt stuff
+
+    snHup->setEnabled(true);
+#endif
+}
+
+void dispatcher::DispatcherWidget::Process() {
+  if(rclcpp::ok()) {
+     online_nodes_ = node_graph_->get_node_names_and_namespaces();
+     for(auto& node : online_nodes_) {
+       std::cout << node.first << node.second << std::endl;
+     }
+     std::cout << std::endl;
+  } else {
+    CFW_INFO("SHUTTING DOWN");
+    QCoreApplication::quit();
+  }
+}
--- src/dispatcher_widget.h
+++ src/dispatcher_widget.h
@@ -5,13 +5,13 @@
 #include <vector>
 
 #include <QGroupBox>
-#include <QLabel>
-#include <QMainWindow>
+#include <QGridLayout>
 #include <QWidget>
-#include <QtGui>
 #include <QTimer>
+#include <QSocketNotifier>
 
 #include <casah_node/casah_node.hpp>
+#include <rclcpp/node_interfaces/node_graph.hpp>
 
 #include "dispatcher/dispatch_item.h"
 
@@ -28,20 +28,43 @@ class DispatcherWidget : public QWidget, CasahNode {
 public:
   explicit DispatcherWidget(QWidget* parent = 0);
   ~DispatcherWidget();
+  QGridLayout* getLayout() {
+    return layout_;
+  }
+  // Unix signal handlers.
+  static void hupSignalHandler(int unused);
+  static void termSignalHandler(int unused);
+
+  const std::vector<std::pair<std::string, std::string>>& getOnlineNodes() {
+    return online_nodes_;
+  }
+
+public slots:
+  // Qt signal handlers.
+  void handleSigHup();
+  void handleSigTerm();
   virtual void Process() override;
 
-private slots:
-
 private:
+  static int sighupFd[2];
+  static int sigtermFd[2];
+
+  QSocketNotifier* snHup = nullptr;
+  QSocketNotifier* snTerm = nullptr;
+
   QTimer* timer_ = nullptr;
+  QGridLayout* layout_ = nullptr;
   std::string dispatcher_config_path_;
   std::vector<dispatcher::DispatchItem*> dispatch_items_;
 
+  std::shared_ptr<rclcpp::node_interfaces::NodeGraph> node_graph_; 
+  std::vector<std::pair<std::string, std::string>> online_nodes_;
+
   QSize minimumSizeHint() const {
-    return QSize(50, 50);
+    return QSize(30, 30);
   }
   QSize sizeHint() const {
-    return QSize(700, 500);
+    return QSize(30, 30);
   }
   void initializeLayout();
   void parseConfig(const std::string&);
