7c9b902ac8a048e3c26f6474ec787bed01ba202a
Kristopher Wehage <kwehage@jpl.nasa.gov>
2021-04-05 23:03:36 -0700

    Add script item

---
+++ src/script_item.h
@@ -0,0 +1,46 @@
+#ifndef SCRIPT_ITEM_H_
+#define SCRIPT_ITEM_H_
+
+#include <vector>
+
+#include <QCheckBox>
+#include <QGroupBox>
+#include <QLabel>
+#include <QWidget>
+
+#include <yaml-cpp/yaml.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <std_msgs/msg/empty.hpp>
+
+namespace dispatcher
+{
+
+class ScriptItem : public QWidget
+{
+  Q_OBJECT  // must be included to add qt meta information
+
+  public: 
+    
+  explicit ScriptItem(QWidget* parent, const YAML::Node&);
+  ~ScriptItem();
+
+  bool is_checked();
+
+ public slots:
+  void StartCb();
+
+ private:
+  std::string       name_;
+  std::string       cmd_;
+  std::string       icon_;
+  int               index_  = -1;
+  QLabel*           label_  = nullptr;
+  bool use_terminal_ = true;
+ signals:
+};
+
+}  // namespace dispatcher
+
+#endif
